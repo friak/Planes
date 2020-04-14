493837f06b0d83f9521e63cf578fa491e777ff5f
friak <friak42@gmail.com>
2020-04-13 19:05:35 -0600

    3.31 Update

---
+++ Planes/Assets/Scripts/bg_rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bg_rotation : MonoBehaviour
+{
+    public float speed = 10f; //degrees per second
+
+    void Update()
+    {
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * speed);
+    }
+
+}
