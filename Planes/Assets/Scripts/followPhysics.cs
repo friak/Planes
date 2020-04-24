6a40042b20c9b56bdc0a430c039fee558b0a44d9
friak <friak42@gmail.com>
2020-04-24 10:50:33 -0600

    Fixed Update, Pinch, World Building Added

--- Planes/Assets/Scripts/followPhysics.cs
+++ Planes/Assets/Scripts/followPhysics.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class followPhysics : MonoBehaviour
 {
 
     public Transform target;
+    //public GameObject child;
     Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        Time.fixedDeltaTime = 0.01f;
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         rb.MovePosition(target.transform.position);
-        //transform.position = target.transform.position;
+        //transform.position = child.transform.position;
     }
 }
---
+++ Planes/Assets/_NetworkManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class _NetworkManager : MonoBehaviourPunCallbacks
+{
+    //instance
+    public static _NetworkManager instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+            gameObject.SetActive(false);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master Server!");
+        CreateRoom("Planes");
+    }
+
+    public void CreateRoom (string roomName)
+    {
+        PhotonNetwork.CreateRoom(roomName);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created room:" + PhotonNetwork.CurrentRoom.Name);
+    }
+    public void JoinRoom (string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void ChangeScene (string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+}
