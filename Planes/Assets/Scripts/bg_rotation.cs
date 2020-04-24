7e04a0a3e7e52e1eb79299fae6496593ccf16b7e
friak <friak42@gmail.com>
2020-04-24 00:57:52 -0600

    adding juice

--- Planes/Assets/Scripts/bg_rotation.cs
+++ Planes/Assets/Scripts/bg_rotation.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class bg_rotation : MonoBehaviour
 {
-    public float speed = 10f; //degrees per second
+    public float speed = 5f; //degrees per second
 
     void Update()
     {
-        transform.RotateAround(transform.position, transform.up, Time.deltaTime * speed);
+        //transform.RotateAround(transform.position, transform.up, Time.deltaTime * speed);
+        transform.Rotate(0, speed, 0 * Time.deltaTime); //rotates 50 degrees per second around z axis
     }
 
 }
