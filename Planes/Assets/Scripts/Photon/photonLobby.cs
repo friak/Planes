ca4694e1d023d0b5ebed2b1672a065484c99e749
friak <friak42@gmail.com>
2020-04-24 23:02:11 -0600

    Networking Works but is funky

---
+++ Planes/Assets/Scripts/Photon/photonLobby.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class photonLobby : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); //Connects to the master photon server
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the Photon master server");
+        EnterRoom();
+    }
+
+    public void EnterRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to join a random game but failed. There must be no open games available");
+        CreateRoom();
+    }
+
+
+    void CreateRoom()
+    {
+        Debug.Log("Tring to create new room");
+        int randomRoomName = Random.Range(0, 1);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 2 };
+        PhotonNetwork.CreateRoom("Planes Room" + randomRoomName, roomOps);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("You have now entered a room");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to create a new room but failed, there must already be a room with the same name");
+        CreateRoom();
+    }
+}
