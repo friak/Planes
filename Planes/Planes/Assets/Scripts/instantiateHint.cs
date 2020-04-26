0c00a454919c5febe9d5fae7ee1f55862a66653b
friak <friak42@gmail.com>
2020-04-26 00:27:44 -0600

    Level Complete
    Except hinges being tested

---
+++ Planes/Planes/Assets/Scripts/instantiateHint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiateHint : MonoBehaviour
+{
+   
+    public GameObject Puzzle;
+    public GameObject Paper;
+
+    public GameObject Hint;
+    public GameObject donePaper;
+    public GameObject doneSil;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I have entered trigger");
+        GameObject otherGO = other.gameObject;
+
+        if (otherGO.name == "grababble_handler")
+        {
+            FindObjectOfType<SoundManager>().Play("next");
+            Puzzle.SetActive(true);
+            Paper.SetActive(true);
+            Hint.SetActive(true);
+            Destroy(donePaper);
+            Destroy(doneSil);
+        }
+
+    }
+}
